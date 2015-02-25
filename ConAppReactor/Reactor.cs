03481d0d79303b13df4a1078054407c5ab94882f
kriss3 <kris_s2@outlook.com>
2015-02-24 20:53:30 -0800

    Adding Reactor class and its implementation

---
+++ ConAppReactor/Reactor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppReactor
+{
+	delegate void MyHandler(string message);
+	class Reactor
+	{
+		public event MyHandler myEvent;
+
+		private int _temp;
+		public int Temperature
+		{
+			get { return _temp; }
+			set
+			{
+				_temp = value;
+				if (_temp > 200)
+				{
+					FireEvent();
+				}
+			}
+		}
+
+		private void FireEvent()
+		{
+			if (myEvent != null)
+			{
+				Console.WriteLine("Reactor is melting with !!!");
+			}
+		}
+	}
+}
